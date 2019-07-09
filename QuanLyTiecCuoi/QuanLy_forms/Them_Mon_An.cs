d3a9f102bfeb94187c1c4fd56afb792d2540caef
nguyenhoang241625532 <49204135+nguyenhoang241625532@users.noreply.github.com>
2019-07-09 20:25:43 +0700

    Add files via upload

---
+++ QuanLyTiecCuoi/QuanLy_forms/Them_Mon_An.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyTiecCuoi.QuanLy_forms
+{
+    public partial class Them_Mon_An : Form
+    {
+        public Them_Mon_An()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ QuanLyTiecCuoi/QuanLy_forms/Them_dich_vu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyTiecCuoi.QuanLy_forms
+{
+    public partial class Them_dich_vu : Form
+    {
+        public Them_dich_vu()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
